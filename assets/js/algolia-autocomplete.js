7d448f227efd76e6eb0ab0e3eb9c62d1b631d6df
Maria Schreiber <meschreiber3@gmail.com>
2018-01-28 14:25:26 -0500

    Polish autocomplete syling

--- assets/js/algolia-autocomplete.js
+++ assets/js/algolia-autocomplete.js
@@ -22,78 +22,88 @@ autocomplete('#aa-search-input', {
             '<div class="right-wrapper"><div class="aa-dataset-iview"></div><div class="aa-dataset-tv"></div></div>'
     }
 }, [{
-        source: autocomplete.sources.hits(articlesIndex, {
-            hitsPerPage: 5,
-            //filter: "lang:en"
-        }),
-        displayKey: 'title_t',
-        name: 'news',
-        templates: {
-            header: '<div id="aa-news-header" class="aa-suggestions-category"><img id="news" img src="homepage/2013/styles/img/news-logo.png" ></div>',
-            //'suggestion' templating function used to render a single suggestion
-            suggestion: function (suggestion) {
-                return '<a href="'+
+    source: autocomplete.sources.hits(articlesIndex, {
+        hitsPerPage: 5,
+        //filter: "lang:en"
+    }),
+    displayKey: 'title_t',
+    name: 'news',
+    templates: {
+        header: '<div id="aa-news-header" class="aa-suggestions-category"><img id="news" img src="homepage/2013/styles/img/news-logo.png" ></div>',
+        //'suggestion' templating function used to render a single suggestion
+        suggestion: function (suggestion) {
+            return '<a href="' +
                 suggestion.canonicalURL +
                 '"><div class="aa-title-wrapper"><span>' + suggestion._highlightResult.title.value + '</span></div></a>'
-            },
-            empty: '<div class="aa-empty">No matching ABC articles.</div>'
-        }
-    },
-    {
-        source: autocomplete.sources.hits(radioIndex, {
-            hitsPerPage: 1
-        }),
-        displayKey: 'title_t',
-        name: 'radio',
-        templates: {
-            //'suggestion' templating function used to render a single suggestion
-            header: '<div id="aa-radio-header" class="aa-suggestions-category"><img src="homepage/2013/styles/img/radio-logo.png" style="height: 40px;"></div>',
-            suggestion: function (suggestion) {
-                return '<a href="'+
+        },
+        empty: '<div class="aa-empty">No matching ABC articles.</div>'
+    }
+},
+{
+    source: autocomplete.sources.hits(radioIndex, {
+        hitsPerPage: 1
+    }),
+    displayKey: 'title_t',
+    name: 'radio',
+    templates: {
+        //'suggestion' templating function used to render a single suggestion
+        header: '<div id="aa-radio-header" class="aa-suggestions-category"><img src="homepage/2013/styles/img/radio-logo.png" style="height: 40px;"></div>',
+        suggestion: function (suggestion) {
+            return '<a href="' +
                 suggestion.canonicalURL +
                 '"><div class="aa-radio-wrapper"><div id="audio-play"></div><span>' + suggestion._highlightResult.title.value + '</span></div></a>'
-            },
-            empty: '<div class="aa-empty">No matching ABC audio.</div>'
-        }
-    },
-    {
-        source: autocomplete.sources.hits(iviewIndex, {
-            hitsPerPage: 3
-        }),
-        displayKey: 'title_t',
-        name: 'iview',
-        templates: {
-            //'suggestion' templating function used to render a single suggestion
-            header: '<div class="aa-suggestions-category"><img img src="homepage/2013/styles/img/iview-logo.png"></div>',
-            suggestion: function (suggestion) {
+        },
+        empty: '<div class="aa-empty">No matching ABC audio.</div>'
+    }
+},
+{
+    source: autocomplete.sources.hits(iviewIndex, {
+        hitsPerPage: 3
+    }),
+    displayKey: 'title_t',
+    name: 'iview',
+    templates: {
+        //'suggestion' templating function used to render a single suggestion
+        header: '<div class="aa-suggestions-category"><img img src="homepage/2013/styles/img/iview-logo.png"></div>',
+        suggestion: function (suggestion) {
+            return '<div class="video-wrapper"><a class="dropdown" href="' + suggestion.canonicalURL + '">' +
+                '<img class="video-thumbnail" src="' + suggestion.media.image.thumbnail.images['16x9'] + '">' +
+                '</a></div>' +
+                '<div class="video-text-wrapper"><div class="video-title iview-title">' + suggestion._highlightResult.series.title.value +
+                ':</div><div class="video-title">' + suggestion._highlightResult.title.value + '</div></div>'
+        },
+        empty: '<div class="aa-empty">No matching iView media.</div>'
+    }
+},
+{
+    source: autocomplete.sources.hits(tvIndex, {
+        hitsPerPage: 3
+    }),
+    displayKey: 'title_t',
+    name: 'tv',
+    templates: {
+        //'suggestion' templating function used to render a single suggestion
+        header: '<div class="aa-suggestions-category"><img img src="homepage/2013/styles/img/television-logo.jpeg"></div>',
+        suggestion: function (suggestion) {
+            try {
                 return '<div class="video-wrapper"><a class="dropdown" href="' + suggestion.canonicalURL + '">' +
-                    '<img class="video-thumbnail" src="' + suggestion.media.image.thumbnail.images['16x9'] + '">' +
-                    '</a></div>' +
-                    '<div class="video-text-wrapper"><div class="video-title iview-title">' + suggestion._highlightResult.series.title.value +
-                    ':</div><div class="video-title">' + suggestion._highlightResult.title.value + '</div></div>'
-            },
-            empty: '<div class="aa-empty">No matching iView media.</div>'
-        }
-    },
-    {
-        source: autocomplete.sources.hits(tvIndex, {
-            hitsPerPage: 3
-        }),
-        displayKey: 'title_t',
-        name: 'tv',
-        templates: {
-            //'suggestion' templating function used to render a single suggestion
-            header: '<div class="aa-suggestions-category"><img img src="homepage/2013/styles/img/television-logo.jpeg"></div>',
-            suggestion: function (suggestion) {
-                return '<div class="video-wrapper"><a class="dropdown" href="' + suggestion.canonicalURL + '">' +
-                    '<img class="video-thumbnail" src="' + suggestion.media.image.poster.images['16x9'] + '">' +
+                    '<img class="video-thumbnail" src="' + suggestion.media.image.poster.images['16x9']  + '">' +
                     '</a></div>' +
                     '<div class="video-title">' + suggestion._highlightResult.title.value +
                     '</div></div>'
-            },
-            empty: '<div class="aa-empty">No matching ABC television shows.</div>'
-        }
-    },
+            } catch(e) {
+                console.log('Images missing from record');
+                return '<div class="video-wrapper"><a class="dropdown" href="' + suggestion.canonicalURL + '">' +
+                    '<img class="video-thumbnail" src=homepage/2013/styles/img/abc-default.png>' +
+                    '</a></div>' +
+                    '<div class="video-title">' + suggestion._highlightResult.title.value +
+                    '</div></div>'
+            }
 
-]);
+        },
+        empty: '<div class="aa-empty">No matching ABC television shows.</div>'
+    }
+},
+
+    ]);
 
