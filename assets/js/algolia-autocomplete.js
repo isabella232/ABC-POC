3b0bd5886fd2349492b17fbd0644db0b0444ae5d
Maria Schreiber <meschreiber3@gmail.com>
2018-02-02 12:52:14 -0500

    Add promotional banner to drop down

--- assets/js/algolia-autocomplete.js
+++ assets/js/algolia-autocomplete.js
@@ -13,19 +13,33 @@ var appID = 'Y63Q32NVDL',
     iviewIndex = client.initIndex(iviewIndexName),
     radioIndex = client.initIndex(radioIndexName),
     tvIndex = client.initIndex(tvIndexName);
+    // articleHitSource = autocomplete.sources.hits(articlesIndex, { hitsPerPage: 5 });
 
 autocomplete('#aa-search-input', {
     debug: true,
     hint: true,
     templates: {
-        dropdownMenu: '<div class="left-wrapper"><div class="aa-dataset-news"></div><div class="aa-dataset-radio"></div></div>' +
+        dropdownMenu: '<div class="left-wrapper"><div id="left-promotional" class="promotional"></div><div class="aa-dataset-news"></div><div class="aa-dataset-radio"></div></div>' +
             '<div class="right-wrapper"><div class="aa-dataset-iview"></div><div class="aa-dataset-tv"></div></div>'
     }
 }, [{
-    source: autocomplete.sources.hits(articlesIndex, {
-        hitsPerPage: 5,
-        //filter: "lang:en"
-    }),
+    source: function(query, callback) {
+        articlesIndex.search(query, { hitsPerPage: 5}).then(function(answer) {
+            $('#left-promotional').empty();
+            if(answer.userData){
+                if (answer.userData[0].canonicalURL) {
+                    var $link = $(`<a href=${answer.userData[0].canonicalURL}></a>`);
+                    var $banner = $('<img class="img-banner" style="width:100%;">').attr('src', `./assets/img/${answer.userData[0].imgSrc}`);
+                    $link.append($banner);
+                    $('#left-promotional').css("display", "inline")
+                    $('#left-promotional').prepend($link);
+                }
+            }
+          callback(answer.hits);
+        }, function() {
+          callback([]);
+        });
+    },
     displayKey: 'title_t',
     name: 'news',
     templates: {
@@ -67,18 +81,18 @@ autocomplete('#aa-search-input', {
         header: '<div class="aa-suggestions-category"><img img src="homepage/2013/styles/img/iview-logo.png"></div>',
         suggestion: function (suggestion) {
             try {
-            return '<div class="video-wrapper"><a class="dropdown" href="' + suggestion.canonicalURL + '">' +
-                '<img class="video-thumbnail" src="https://res.cloudinary.com/algolia-maria/image/fetch/' + suggestion.media.image.thumbnail.images['16x9'] + '">' +
-                '</a></div>' +
-                '<div class="video-text-wrapper"><div class="video-title iview-title">' + suggestion._highlightResult.series.title.value +
-                ':</div><div class="video-title">' + suggestion._highlightResult.title.value || '' + '</div></div>'
-            } catch(e) {
+                return '<div class="video-wrapper"><a class="dropdown" href="' + suggestion.canonicalURL + '">' +
+                    '<img class="video-thumbnail" src="https://res.cloudinary.com/algolia-maria/image/fetch/' + suggestion.media.image.thumbnail.images['16x9'] + '">' +
+                    '</a></div>' +
+                    '<div class="video-text-wrapper"><div class="video-title iview-title">' + suggestion._highlightResult.series.title.value +
+                    ':</div><div class="video-title">' + suggestion._highlightResult.title.value || '' + '</div></div>'
+            } catch (e) {
                 console.log('Images missing from record');
                 return '<div class="video-wrapper"><a class="dropdown" href="' + suggestion.canonicalURL + '">' +
-                '<img class="video-thumbnail" src="homepage/2013/styles/img/abc-default.png"">' +
-                '</a></div>' +
-                '<div class="video-text-wrapper"><div class="video-title iview-title">' + suggestion._highlightResult.series.title.value +
-                ':</div><div class="video-title">' + '' + '</div></div>'
+                    '<img class="video-thumbnail" src="homepage/2013/styles/img/abc-default.png"">' +
+                    '</a></div>' +
+                    '<div class="video-text-wrapper"><div class="video-title iview-title">' + suggestion._highlightResult.series.title.value +
+                    ':</div><div class="video-title">' + '' + '</div></div>'
             }
         },
         empty: '<div class="aa-empty">No matching iView media.</div>'
@@ -96,11 +110,11 @@ autocomplete('#aa-search-input', {
         suggestion: function (suggestion) {
             try {
                 return '<div class="video-wrapper"><a class="dropdown" href="' + suggestion.canonicalURL + '">' +
-                    '<img class="video-thumbnail" src="https://res.cloudinary.com/algolia-maria/image/fetch/' + suggestion.media.image.poster.images['16x9']  + '">' +
+                    '<img class="video-thumbnail" src="https://res.cloudinary.com/algolia-maria/image/fetch/' + suggestion.media.image.poster.images['16x9'] + '">' +
                     '</a></div>' +
                     '<div class="video-title">' + suggestion._highlightResult.title.value +
                     '</div></div>'
-            } catch(e) {
+            } catch (e) {
                 console.log('Images missing from record');
                 return '<div class="video-wrapper"><a class="dropdown" href="' + suggestion.canonicalURL + '">' +
                     '<img class="video-thumbnail" src="homepage/2013/styles/img/abc-default.png">' +
--- assets/js/banner-widget.js
+++ assets/js/banner-widget.js
@@ -1,12 +1,13 @@
 var bestBetWidget = {
     render: function (options) {
         $(".img-banner").remove();
+        console.log(options.results);
         var userData = options.results._rawResults[0].userData;
         if (userData) {
             if (userData[0].canonicalURL) {
                 var $link = $(`<a href=${userData[0].canonicalURL}></a>`);
                 var $banner = $('<img class="img-banner" style="width:100%;">').attr('src', `./assets/img/${userData[0].imgSrc}`);
-                $link.append($banner);
+                $link.append($banner);''
                 $('.hitsContainer').prepend($link);
 
             }
